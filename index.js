fafceda8574c15454b031c51e22b4a00e90f80f6
Bingdididi <bzhou26@my.bcit.ca>
2023-04-26 20:48:52 -0700

    modify the expiration time

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const path = require('path');
 
 
 
-const expireTime = 24 * 60 * 60 * 1000; //expires after 1 day  (hours * minutes * seconds * millis)
+const expireTime = 1 * 60 * 60 * 1000; //expires after 1 hour (hours * minutes * seconds * millis)
 
 /* secret information section */
 const mongodb_host = process.env.MONGODB_HOST;
